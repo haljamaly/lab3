f2b49f0494a28bd22b7d1eda49070455b155c227
Hasan Al - Jamaly <haljamal@ucsd.edu>
2018-01-24 23:04:58 -0800

    Lab Done

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -4,15 +4,32 @@
 $(document).ready(function() {
 	initializePage();
 })
-
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$(this).text("Your edges are officially SNATCHED.");
+		$(".jumbotron p").addClass("active");
+		$("").toggleClass("active");
 	});
 
+	$(".project").click(projectClick);
+
+}
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
-}
+function projectClick(e) {
+	e.preventDefault();
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+	if (description.length % 2 == 0 ) {
+		$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+	}
+	else {
+		description.fadeToggle();
+
+	}
+
+	}
